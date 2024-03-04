3f6f74d672fbd8466060328bc6e3bf85951cdadb
Raja Mukherji <rajamukherji@gmail.com>
2024-03-04 08:47:05 +0000

    Updates.

---
+++ schemas/upgrade-1.sql
@@ -0,0 +1,36 @@
+ALTER TABLE registries RENAME TO registries_old;
+
+CREATE TABLE registries(
+	id INTEGER PRIMARY KEY,
+	name STRING,
+	url STRING,
+	user STRING,
+	password STRING,
+	timeout INTEGER
+);
+
+INSERT INTO registries(name, url, user, password, timeout) SELECT
+	name, url, user, password, timeout
+FROM
+	registries_old;
+
+DROP TABLE registries_old;
+
+CREATE TABLE destinations(
+	project INTEGER,
+	registry INTEGER,
+	tag STRING
+);
+
+INSERT INTO destinations(project, registry, tag) SELECT
+	p.id, r.id, p.tag
+FROM
+	projects p, registries r
+WHERE
+	p.destination = r.name;
+
+
+ALTER TABLE projects DROP COLUMN destination;
+ALTER TABLE projects DROP COLUMN tag;
+
+UPDATE config SET value = 2 WHERE name = 'version';
